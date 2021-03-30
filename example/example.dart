d160e09c06fceab6567bca5c4475bc9edc287ea1
rxlabz <erick@rxlabz.com>
2021-03-30 13:02:25 +0200

    update /example

--- example/example.dart
+++ example/example.dart
@@ -6,42 +6,51 @@ Future main() async {
   // Open a connection (testdb should already exist)
   final conn = await MySqlConnection.connect(
     ConnectionSettings(
-      host: 'localhost',
-      port: 3306,
+      host: 'localhost' /* or path to a socket file */,
+      port: 3306 /* default */,
       user: 'root',
       password: 'root',
       db: 'testdb',
     ),
+    isUnixSocket: false /* default */,
   );
 
   // Create a table
   await conn.query(
-    'CREATE TABLE trucs (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), email varchar(255), age int)',
+    'CREATE TABLE users (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), email varchar(255), age int)',
   );
 
   // Insert some data
   var result = await conn.query(
-    'insert into trucs (name, email, age) values (?, ?, ?)',
+    'insert into users (name, email, age) values (?, ?, ?)',
     ['Bob', 'bob@bob.com', 25],
   );
-  print('Inserted row id=${result.insertId}');
+
+  final insertId = result.insertId;
+
+  if (insertId == null) {
+    print('Insertion failed !');
+    return;
+  }
+
+  print('Inserted row id=$insertId');
 
   // Query the database using a parameterized query
   var results = await conn.query(
-    'select name, email, age from trucs where id = ?',
-    [result.insertId!],
+    'select name, email, age from users where id = ?',
+    [insertId],
   );
   for (var row in results) {
     print('Name: ${row[0]}, email: ${row[1]} age: ${row[2]}');
   }
 
   // Update some data
-  await conn.query('update trucs set age=? where name=?', [26, 'Bob']);
+  await conn.query('update users set age=? where name=?', [26, 'Bob']);
 
   // Query again database using a parameterized query
   var results2 = await conn.query(
-    'select name, email, age from trucs where id = ?',
-    [result.insertId!],
+    'select name, email, age from users where id = ?',
+    [insertId],
   );
   for (var row in results2) {
     print('Name: ${row[0]}, email: ${row[1]} age: ${row[2]}');
